59047d8d21976b70946461c90350eebd844e450e
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-28 11:31:22 +0530

    new1

---
+++ Activity2.py
@@ -0,0 +1,26 @@
+#First part
+a=14
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+
+#Second paet
+a="python coding"
+b="Happy"
+if a!=b:
+    print(a,"and",b,"are different") 
+
+#Third part
+a=20
+b=24
+if (a==26)!=(b==52):
+    print("Hello")
+
+#Even and odd number
+x=int(input("Enter any number"))
+if x%2!=0:
+    print("The number is a odd number")
+
+
+
